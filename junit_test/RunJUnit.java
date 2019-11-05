cfe1f4c214e11b547b8d5626fe8b9a048195a668
Nkenta Uchechukwu Ferdinand <35242073+nkenta@users.noreply.github.com>
2019-11-05 11:04:33 +0100

    Minor Text Update

--- junit_test/RunJUnit.java
+++ junit_test/RunJUnit.java
@@ -15,7 +15,7 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;		
 import org.junit.runner.notification.Failure;		
 
-public class RunJUnits {				
+public class RunJUnit {				
 			public static void main(String[] args) {									
       Result result = JUnitCore.runClasses(Junit4AssertionTest.class);					
 			for (Failure failure : result.getFailures()) {							
